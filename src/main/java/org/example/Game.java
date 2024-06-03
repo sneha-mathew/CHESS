c3a2ce31aac5310cc7809e2de047e10253ca84d0
Sneha Mathew <72590393+sneha-mathew@users.noreply.github.com>
2024-06-03 20:58:10 +0530

    Main functionality

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import org.example.board.Position;
+import org.example.exception.InvalidPostionException;
+import org.example.exception.UnknownPiece;
+import org.example.pieces.Piece;
+import org.example.pieces.PieceFactory;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
+        System.out.println("Mention the piece, the color and the position");
+        Scanner sc = new Scanner(System.in);
+        String pieceType = sc.next();
+        String colour = sc.next();
+        String pos = sc.next();
+        processInput(pieceType, colour, pos);
+    }
 
+    private static void processInput(String pieceType, String colour, String pos) {
+        try {
+            List<Position> positionList = PieceFactory.getPiece(pieceType, ("white").equals(colour.toLowerCase()), pos).getPossibleMoves();
+            System.out.println("The possible moves are " + PositionHelper.getStringList(positionList));
+        } catch (UnknownPiece | InvalidPostionException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
